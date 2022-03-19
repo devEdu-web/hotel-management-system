2ac4ffa1bb21fd5e4feed91f0710d4510c83c174
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-03-19 20:06:23 -0300

    employee model: employee schema implemented

---
+++ src/components/employees/Employee.js
@@ -0,0 +1,17 @@
+const {Schema} = require('mongoose')
+const mongoose = require('mongoose')
+
+const employeeSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phone: Number,
+    department: String,
+    address: {
+        country: String,
+        city: String,
+        street: String,
+        zipCode: Number
+    }
+})
+
+module.exports = mongoose.model('Employee', employeeSchema)
